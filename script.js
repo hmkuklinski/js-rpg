1a31eb622f299a6302983ae763b8f472e92b6991
hmkuklinski <hmkuklinski@gmail.com>
2023-10-06 01:35:25 -0400

    added the weapons array
    gave weapon types and power values

--- script.js
+++ script.js
@@ -17,6 +17,24 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterNameText = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const weapons = [
+    {
+        name: "stick",
+        power: 5
+    }, 
+    {
+        name: "dagger",
+        power: 30
+    },
+    {
+        name: "claw hammer",
+        power: 50
+    },
+    {
+        name: "sword",
+        power: 100
+    }
+];
 
 const locations = [
     {
