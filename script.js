4e314e01ba84b86694d84d73cee878816cfe03bd
hmkuklinski <hmkuklinski@gmail.com>
2023-10-06 01:56:32 -0400

    created an array for the monsters and their listed stats

--- script.js
+++ script.js
@@ -36,6 +36,24 @@ const weapons = [
     }
 ];
 
+const monsters = [
+    {
+        name: "slime", 
+        level: 2, 
+        health: 15
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 50
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300
+    }
+];
+
 const locations = [
     {
         name: "town square",
